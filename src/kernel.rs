68891660b5b6f0c0c682916a7e7c9771e29e2b22
Dorian Kostecki <dorian.kostecki@gmail.com>
2024-05-07 23:46:20 +0200

    Improve allocator

--- src/kernel.rs
+++ src/kernel.rs
@@ -1,6 +1,5 @@
 use crate::driver::acpi::Acpi;
 use crate::driver::apic::Apic;
-use crate::memory::MemoryManager;
 use alloc::sync::Arc;
 use spin::RwLock;
 use x86_64::structures::DescriptorTablePointer;
@@ -8,6 +7,5 @@ use x86_64::structures::DescriptorTablePointer;
 pub struct Kernel {
     pub acpi: Arc<Acpi>,
     pub apic: Arc<RwLock<Apic>>,
-    pub memory_manager: Arc<RwLock<MemoryManager>>,
     pub gdt: DescriptorTablePointer,
 }
