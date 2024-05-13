ae451bb393ef47a3cca9a8c0f7d7c93bf926e1a7
Dorian Kostecki <dorian.kostecki@gmail.com>
2024-05-13 22:34:15 +0200

    Initialize FPU, enable OSFXSR and OSXMMEXCPT

--- kernel/src/arch/x86/mod.rs
+++ kernel/src/arch/x86/mod.rs
@@ -1,7 +1,23 @@
 pub mod asm;
 pub mod idt;
 
+use core::arch;
+use x86_64::registers::control::{Cr0, Cr0Flags, Cr4, Cr4Flags};
+
 pub fn perform_arch_initialization() {
     // @TODO: GDT
     idt::init_idt();
+
+    unsafe {
+        Cr0::write(
+            Cr0::read().difference(Cr0Flags::EMULATE_COPROCESSOR)
+                | Cr0Flags::NUMERIC_ERROR
+                | Cr0Flags::MONITOR_COPROCESSOR,
+        );
+        // We don't really need to check whether SSE and SSE2 is present as long mode requires them.
+        // We wouldn't even get here without those extensions.
+        Cr4::write(Cr4::read() | Cr4Flags::OSFXSR | Cr4Flags::OSXMMEXCPT_ENABLE);
+
+        arch::asm!("fninit");
+    }
 }
