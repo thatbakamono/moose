f601400241d2b4bd009308899d7f9270951bf535
error56 <46536532+error56@users.noreply.github.com>
2024-05-14 15:56:50 +0200

    I/O APIC driver (#35)

--- src/driver/apic/mod.rs
+++ src/driver/apic/mod.rs
@@ -1,82 +1,29 @@
+mod io_apic;
+mod local_apic;
+
+pub use io_apic::*;
+pub use local_apic::*;
+
 use crate::arch::x86::idt::IDT;
-use crate::cpu::ProcessorControlBlock;
 use crate::driver::acpi::{Acpi, MadtEntryInner};
 use crate::driver::pit::PIT;
 use crate::kernel::Kernel;
-use crate::memory::{
-    memory_manager, Frame, MemoryError, Page, PageFlags, PhysicalAddress, VirtualAddress, PAGE_SIZE,
-};
+use crate::memory::{memory_manager, Page, PageFlags, VirtualAddress, PAGE_SIZE};
 use alloc::alloc::alloc_zeroed;
 use alloc::sync::Arc;
+use alloc::vec::Vec;
 use core::alloc::Layout;
 use core::arch::asm;
 use core::ptr;
-use log::{debug, info};
+use log::debug;
 use raw_cpuid::CpuId;
 use spin::RwLock;
 use x86_64::instructions::interrupts::without_interrupts;
-use x86_64::registers::control::{Cr4, Cr4Flags};
-use x86_64::structures::idt::InterruptStackFrame;
-
-const LOCAL_APIC_LAPIC_ID_REGISTER: u32 = 0x20;
-const LOCAL_APIC_LAPIC_VERSION_REGISTER: u32 = 0x23;
-// 0x40-0x70 - Reserved
-const LOCAL_APIC_TASK_PRIORITY_REGISTER: u32 = 0x80;
-const LOCAL_APIC_ARBITRATION_PRIORITY_REGISTER: u32 = 0x90;
-const LOCAL_APIC_PROCESSOR_PRIORITY_REGISTER: u32 = 0xA0;
-const LOCAL_APIC_END_OF_INTERRUPT_REGISTER: u32 = 0xB0;
-const LOCAL_APIC_REMOTE_READ_REGISTER: u32 = 0xC0;
-const LOCAL_APIC_LOGICAL_DESTINATION_REGISTER: u32 = 0xD0;
-const LOCAL_APIC_DESTINATION_FORMAT_REGISTER: u32 = 0xE0;
-const LOCAL_APIC_SPURIOUS_INTERRUPT_VECTOR_REGISTER: u32 = 0xF0;
-// ISR
-// TMR
-// IRR
-const LOCAL_APIC_ERROR_STATUS_REGISTER: u32 = 0x280;
-const LOCAL_APIC_INTERRUPT_OPTIONS_REGISTER: u32 = 0x300;
-const LOCAL_APIC_INTERRUPT_TARGET_PROCESSOR_REGISTER: u32 = 0x310;
-const LOCAL_APIC_LVT_TIMER_REGISTER: u32 = 0x320;
-const LOCAL_APIC_LVT_ERROR_REGISTER: u32 = 0x370;
-const LOCAL_APIC_INITIAL_COUNT_REGISTER: u32 = 0x380;
-const LOCAL_APIC_CURRENT_COUNT_REGISTER: u32 = 0x390;
-const LOCAL_APIC_DIVIDE_CONFIGURATION_REGISTER: u32 = 0x3E0;
-const IA32_APIC_BASE_MSR: u32 = 0x1B;
-const APIC_BASE_MSR_BSP_FLAG: u64 = 1 << 8;
-const APIC_BASE_MSR_APIC_GLOBAL_ENABLE_FLAG: u64 = 1 << 11;
-const APIC_BASE_MSR_APIC_BASE_FIELD_MASK: u64 = 0xFFFFFF000;
-
-const STACK_SIZE: usize = 4 * 1024 * 1024;
-const TIMER_IRQ: u32 = to_irq_number(IrqLevel::Clock, IrqId(0)) as u32;
-const LOCAL_APIC_TIMER_PERIODIC: u32 = 1 << 17;
-
-static TRAMPOLINE_CODE: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/trampoline"));
-static mut AP_STARTUP_SPINLOCK: RwLock<u8> = RwLock::new(0);
-
-pub unsafe extern "C" fn ap_start(apic_processor_id: u64, kernel_ptr: *const RwLock<Kernel>) -> ! {
-    // @TODO: Move to perform_arch_initialization()
-    let kernel = Arc::from_raw(kernel_ptr);
-
-    IDT.load();
-    Cr4::write(Cr4::read() | Cr4Flags::FSGSBASE);
-
-    ProcessorControlBlock::create_pcb_for_current_processor(apic_processor_id as u16);
-    let pcb = ProcessorControlBlock::get_pcb_for_current_processor();
-    let local_apic = LocalApic::initialize_for_current_processor(Arc::clone(&kernel));
-    local_apic.enable_timer();
-
-    _ = (*pcb).local_apic.set(local_apic);
-
-    *AP_STARTUP_SPINLOCK.write() = 1;
-    info!("Processor {:p} has started", pcb);
-
-    loop {
-        asm!("hlt");
-    }
-}
 
 pub struct Apic {
     pub local_apic_timer_ticks_per_second: u64,
     pub acpi: Arc<Acpi>,
+    io_apics: Vec<IoApic>,
 }
 
 impl Apic {
@@ -90,10 +37,42 @@ impl Apic {
 
         unsafe { IDT[TIMER_IRQ as u8].set_handler_fn(timer_interrupt_handler) };
 
+        let io_apics = acpi
+            .madt
+            .entries
+            .iter()
+            .filter_map(|entry| match &entry.inner {
+                MadtEntryInner::IoApic(io_apic) => Some(io_apic),
+                _ => None,
+            })
+            .map(|entry| IoApic::new(entry.clone()))
+            .collect();
+
         Apic {
             local_apic_timer_ticks_per_second: 0,
             acpi,
+            io_apics,
         }
+    }
+
+    pub fn redirect_interrupt(&self, redirection_entry: RedirectionEntry, irq: u8) {
+        let io_apic: IoApic = self
+            .io_apics
+            .clone()
+            .into_iter()
+            .filter(|apic| {
+                let start = apic.madt_io_apic.global_system_interrupt_base;
+                let end = start + apic.get_redirection_entry_count();
+
+                (start..end).contains(&(irq as u32))
+            })
+            .next()
+            .unwrap();
+
+        io_apic.redirect_interrupt(
+            redirection_entry,
+            irq - io_apic.madt_io_apic.global_system_interrupt_base as u8,
+        );
     }
 
     pub fn setup_other_application_processors(
@@ -106,9 +85,8 @@ impl Apic {
             let mut memory_manager = memory_manager().write();
 
             memory_manager
-                .map(
+                .map_identity(
                     &Page::new(VirtualAddress::new(0x8000)),
-                    &Frame::new(PhysicalAddress::new(0x8000)),
                     PageFlags::WRITABLE | PageFlags::EXECUTABLE,
                 )
                 .unwrap();
@@ -275,147 +253,6 @@ impl Apic {
             );
             unsafe { PIT.wait_sixteen_millis() }
         }
-    }
-}
-
-pub struct LocalApic {
-    local_apic_base: u64,
-    kernel: Arc<RwLock<Kernel>>,
-}
-
-impl LocalApic {
-    pub fn initialize_for_current_processor(kernel: Arc<RwLock<Kernel>>) -> LocalApic {
-        let apic_base =
-            unsafe { x86_64::registers::model_specific::Msr::new(IA32_APIC_BASE_MSR).read() };
-        let local_apic_base = apic_base & APIC_BASE_MSR_APIC_BASE_FIELD_MASK;
-
-        // Make sure local apic base is mapped into memory
-        // It is always on 4KiB boundary
-        {
-            let mut memory_manager = memory_manager().write();
-
-            match unsafe {
-                memory_manager.map(
-                    &Page::new(VirtualAddress::new(local_apic_base)),
-                    &Frame::new(PhysicalAddress::new(local_apic_base)),
-                    PageFlags::WRITABLE | PageFlags::WRITE_THROUGH | PageFlags::DISABLE_CACHING,
-                )
-            } {
-                Ok(()) => {}
-                Err(MemoryError::AlreadyMapped) => {}
-                Err(err) => {
-                    panic!("{}", err);
-                }
-            }
-        }
-
-        let apic = LocalApic {
-            local_apic_base,
-            kernel,
-        };
-
-        // Enable Local APIC
-        //
-        // Local APIC can be enabled by setting 8th bit of spurious interrupt vector register
-        apic.write_register(
-            LOCAL_APIC_SPURIOUS_INTERRUPT_VECTOR_REGISTER,
-            apic.read_register(LOCAL_APIC_SPURIOUS_INTERRUPT_VECTOR_REGISTER) | (1 << 8),
-        );
-
-        // Remap spurious interrupt vector register
-        apic.write_register(LOCAL_APIC_LVT_ERROR_REGISTER, 0x1F);
-
-        if apic_base & APIC_BASE_MSR_BSP_FLAG != 0 {
-            // We're running first LocalAPIC initialization on the bootstrap processor and need to
-            // check the speed of APIC timer.
-            apic.check_timer_speed()
-        }
-
-        apic
-    }
-
-    pub fn enable_timer(&self) {
-        // Fire timer every 10ms
-        let ticks_per_10ms = self
-            .kernel
-            .read()
-            .apic
-            .read()
-            .local_apic_timer_ticks_per_second
-            / 100;
-
-        // Enable interrupts
-        self.write_register(LOCAL_APIC_TASK_PRIORITY_REGISTER, 0);
-
-        // Set divider 16
-        self.write_register(LOCAL_APIC_DIVIDE_CONFIGURATION_REGISTER, 0x3);
-
-        self.write_register(
-            LOCAL_APIC_LVT_TIMER_REGISTER,
-            TIMER_IRQ | LOCAL_APIC_TIMER_PERIODIC,
-        );
-
-        // Start the timer
-        self.write_register(LOCAL_APIC_INITIAL_COUNT_REGISTER, ticks_per_10ms as u32);
-    }
-
-    pub fn signal_end_of_interrupt(&self) {
-        self.write_register(LOCAL_APIC_END_OF_INTERRUPT_REGISTER, 0);
-    }
-
-    pub fn reset_error_register(&self) {
-        self.write_register(LOCAL_APIC_ERROR_STATUS_REGISTER, 0);
-    }
-
-    fn check_timer_speed(&self) {
-        // This function is run only once during BSP's Local APIC initialization
-
-        // APIC timer tick speed is not standardized, and every platform can have custom speed, so
-        // we need to somehow measure it.
-        //
-        // It can be done by running APIC timer, sleeping for measurable amount of time (with use of
-        // PIT) and checking how many times APIC "ticked".
-
-        // Tell APIC timer to use divider 16
-        self.write_register(LOCAL_APIC_DIVIDE_CONFIGURATION_REGISTER, 0x3);
-
-        // Set APIC timer init counter to -1
-        //
-        // After every write to this register, current countdown is discarded and new initial count
-        // is copied to current count register and countdown starts.
-        self.write_register(LOCAL_APIC_INITIAL_COUNT_REGISTER, 0xFFFFFFFF);
-
-        // Perform PIT-assisted sleep for 1 second
-        unsafe { PIT.wait_seconds(1) };
-
-        let ticks_per_second = 0xFFFFFFFF - self.read_register(LOCAL_APIC_CURRENT_COUNT_REGISTER);
-
-        self.kernel
-            .read()
-            .apic
-            .write()
-            .local_apic_timer_ticks_per_second = ticks_per_second as u64;
-    }
-
-    fn read_register(&self, register: u32) -> u32 {
-        let ptr = (self.local_apic_base + register as u64) as *mut u32;
-        unsafe { ptr::read_volatile(ptr) }
-    }
-
-    fn write_register(&self, register: u32, value: u32) {
-        let ptr = (self.local_apic_base + register as u64) as *mut u32;
-
-        unsafe { ptr::write_volatile(ptr, value) }
-    }
-}
-
-extern "x86-interrupt" fn timer_interrupt_handler(_interrupt_stack_frame: InterruptStackFrame) {
-    unsafe {
-        _ = &(*ProcessorControlBlock::get_pcb_for_current_processor())
-            .local_apic
-            .get()
-            .unwrap()
-            .signal_end_of_interrupt();
     }
 }
 
